ed4943cff4733064def8262cfb5ca1cd62f6cbe9
Hyeon-Uk <rlagusdnr120@gmail.com>
2022-11-29 23:01:17 +0900

    part1-2

---
+++ ex2/README.md
@@ -0,0 +1,12 @@
+## JPA란 무엇인가?
+
+- Java Persistence API 의 약자입니다.
+- Java 언어를 통해서 데이터베이스와 같은 영속 계층을 처리하고자 하는 스펙기준입니다.
+
+## ORM이란?
+
+- Object-Relational Mapping의 약자로, 객체지향 패러다임을 관계형 데이터베이스에 보존하는 기술.
+- 객체지향 패러다임을 관계형 패러다임으로 매핑해주는 개념
+
+## Spring Data JPA란?
+- Hibernate를 스프링부트에서 쉽게 사용할 수 있는 추가적인 API를 제공하는 라이브러리
---
+++ ex2/src/main/java/org/zerock/ex2/repository/MemoRepository.java
@@ -0,0 +1,44 @@
+package org.zerock.ex2.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+import org.zerock.ex2.entity.Memo;
+
+import java.util.List;
+
+public interface MemoRepository extends JpaRepository<Memo, Long> {
+    List<Memo> findByMnoBetweenOrderByMnoDesc(Long from, Long to);
+
+    Page<Memo> findByMnoBetween(Long from, Long to, Pageable pageable);
+
+    void deleteMemoByMnoLessThan(Long num);
+
+    @Query("SELECT m FROM Memo m order by m.mno desc")
+    List<Memo> getListDesc();
+
+    @Transactional
+    @Query("update Memo m set m.memoText = :memoText where m.mno = :mno")
+    @Modifying
+    int updateMemoText(@Param("mno") Long mno, @Param("memoText") String memoText);
+
+    @Transactional
+    @Query("update Memo m set m.memoText = :#{#memo.memoText} where m.mno = :#{#memo.mno}")
+    @Modifying
+    int updateMemoText(@Param("memo") Memo memo);
+
+    @Query(value = "select m from Memo m where m.mno > :mno"
+            ,countQuery = "select count(m) from Memo m where m.mno > :mno")
+    Page<Memo> getListWithQuery(Long mno,Pageable pageable);
+
+    @Query(value="select m.mno,m.memoText,CURRENT_DATE from Memo m where m.mno>:mno"
+            ,countQuery = "select count(m) from Memo m where m.mno > :mno")
+    Page<Object[]> getListWithQueryObject(Long mno,Pageable pageable);
+
+    @Query(value="select * from tbl_memo where mno > 0",nativeQuery = true)
+    List<Object[]> getNativeResult();
+}
