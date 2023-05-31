f080796e460c4fe646ecb8bccae3d4228f51f8a3
Hyeon-Uk <rlagusdnr120@gmail.com>
2023-05-31 19:00:18 +0900

    refactor : 다시 시작하기 위해 리셋

--- board/src/main/java/org/zerock/board/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package org.zerock.board.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.zerock.board.entity.Member;
-
-public interface MemberRepository extends JpaRepository<Member,String> {
-}
