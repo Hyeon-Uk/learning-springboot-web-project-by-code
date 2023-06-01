71bf3ea7913ef7895ec137c30eab0a4421a729e5
Hyeon-Uk <rlagusdnr120@gmail.com>
2023-06-01 20:59:31 +0900

    clear : chapter3

---
+++ ex3/src/main/java/com/example/dto/SampleDTO.java
@@ -0,0 +1,15 @@
+package com.example.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder(toBuilder = true)
+public class SampleDTO {
+    private Long sno;
+    private String first;
+    private String last;
+    private LocalDateTime regTime;
+}
